aefd2d1bd59515d1f6b7f8bb1f3257f6a3e4cacf
Telaviv2 <briancode132@gmail.com>
2025-01-06 01:45:55 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ const fetch = require('node-fetch');
 const cheerio = require('cheerio');
 
 // Ganti dengan token bot Anda
-const bot = new Bot('7897377:AAFJXMrPc7SGoQ2nfWT5fqJ0_uHT-4qfnFY');
+const bot = new Bot('7956762549:AAFLGuXP8lJT1vkuEiNuTXmQ43OSdp2lS3s');
 
 // Fungsi untuk mendapatkan daftar video hentai
 async function getHentaiList() {
